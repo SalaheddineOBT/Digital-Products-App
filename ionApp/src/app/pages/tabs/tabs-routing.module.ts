74bb951a7f79593f17d91072de09d715f0e072ef
salaheddineobt <salaheddine.ouladbentaib@gmail.com>
2022-07-01 17:57:56 +0100

    save

--- ionApp/src/app/pages/tabs/tabs-routing.module.ts
+++ ionApp/src/app/pages/tabs/tabs-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
         component: TabsPage,
         children: [
             {
-                path: 'tab1',
-                loadChildren: () => import('../../tab1/tab1.module').then(m => m.Tab1PageModule)
+                path: 'home',
+                loadChildren: () => import('../../screens/home/home.module').then(m => m.HomePageModule)
             },
             {
                 path: 'tab2',
@@ -28,7 +28,7 @@ const routes: Routes = [
     },
     {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full'
     }
 ];
