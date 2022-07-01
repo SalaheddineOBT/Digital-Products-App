86352092ba95c7d6c5dbe4674a927794c3295192
salaheddineobt <salaheddine.ouladbentaib@gmail.com>
2022-07-01 17:22:24 +0100

    save

--- ionApp/src/app/app-routing.module.ts
+++ ionApp/src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
   }
 ];
 @NgModule({
