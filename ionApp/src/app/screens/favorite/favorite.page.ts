cbb3cc7887d9b52bf914fe52618eb005e8d01cc6
salaheddineobt <salaheddine.ouladbentaib@gmail.com>
2022-07-07 11:30:42 +0100

    save

--- ionApp/src/app/screens/favorite/favorite.page.ts
+++ ionApp/src/app/screens/favorite/favorite.page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-favorite',
-  templateUrl: './favorite.page.html',
-  styleUrls: ['./favorite.page.scss'],
+    selector: 'app-favorite',
+    templateUrl: './favorite.page.html',
+    styleUrls: ['./favorite.page.scss'],
 })
 export class FavoritePage implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
