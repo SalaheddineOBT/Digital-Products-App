c872aa250509622dcc436279b729080f0af00860
salaheddineobt <salaheddine.ouladbentaib@gmail.com>
2022-07-17 19:57:40 +0000

    save changes

--- myapi/api/auth/home.php
+++ myapi/api/auth/home.php
@@ -4,4 +4,29 @@
     include_once '../config/cors.php';
     include_once '../../vendor/autoload.php';
 
+    use \Firebase\JWT\JWT;
+    use Firebase\JWT\Key;
+
+    $db=new DataBase();
+    $allHeader = getallheaders();
+
+    if(isset($allHeader['Authorization']) && $_SERVER["REQUEST_METHOD"] == 'POST'):
+        $token = explode(" ",$allHeader['Authorization'])[1];
+        try{
+            $key = "Sgenius%@obt2001#";
+            $decode = JWT::decode($token,new Key($key, 'HS256'));
+            echo json_encode(array(
+                'success'=>1,
+                'status'=>200,
+                'message'=>'Token Decoded SuccessFully .',
+                'decode'=>$decode
+            ));
+        }catch(Exception $e){
+            $db->Message(0,500,"".$e);
+        }
+    else:
+        http_response_code(100);
+        $db->Message(0,500,"Unauthorized !");
+    endif;
+
 ?>
